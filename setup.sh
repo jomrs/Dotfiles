d8f1b7545e3b3847417247a53d3f46c80dc80f8e
Jomrs <jonathanmotareis@gmail.com>
2022-07-16 13:34:31 -0300

    improved to automate the setup.sh bash script.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-#Acutal Path
+#Acutal Path.
 CURRENT_PATH=$PWD
+#Folders to Restore Ignoring sh setup file.
+CONFIGS_FOLDER=(`ls -I '*.sh'`)
 
 echo '⚡Restoring default config...'
 sleep 1
 
-cp -r "${CURRENT_PATH}/bspwm" ~/.config
+#Loop through folders to copy them.
+for ((i=0; i < ${#CONFIGS_FOLDER[@]}; i++)); do
+	echo "🔵 Currently restoring ${CONFIGS_FOLDER[$i]}..."
+	cp -r "${CURRENT_PATH}/${CONFIGS_FOLDER}" ~/.config
+	sleep 1
+done
+
 echo '✅ Finished.'
 
